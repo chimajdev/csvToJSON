8e047ff53a903c4ca177b86f16e035191837ae47
JayWhat <55168396+chimajdev@users.noreply.github.com>
2020-04-30 07:50:06 -0400

    Update problem1.sh

--- problem1.sh
+++ problem1.sh
@@ -5,3 +5,4 @@ mkdir outputprob1
 touch ./testproblem1/test.csv ./testproblem1/test.txt
 cp ./testproblem1/test.csv ./outputprob1
 zip problem1.zip outputprob1
+scp problem1.zip user@remote.server.fi:/path/to/location
