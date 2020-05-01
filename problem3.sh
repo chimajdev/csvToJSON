cbc081fc5ad28cc58aee18e9c25b9a1ab1d86781
JayWhat <55168396+chimajdev@users.noreply.github.com>
2020-05-01 14:33:15 -0400

    Update problem3.sh

--- problem3.sh
+++ problem3.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 url=https://dashboard.healthit.gov/api/open-api.php?source=workforce-programs-trained.csv
 # test url
+
+apt-get install curl -y
+# Install curl
+
 curl ${url} > outfile.csv
 # Download file
